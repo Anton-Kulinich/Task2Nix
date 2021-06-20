8eddb6d8e6ae109fbf469a01fb91f1b1c40299d5
Anton <Anton@DESKTOP-RPNK5SQ>
2021-06-20 21:44:42 +0300

    Добавьте файлы проекта.

---
+++ Task2Nix/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task2Nix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=DESKTOP-RPNK5SQ\SQLEXPRESS;Initial Catalog=task2db;Integrated Security=True";
+            DataContext dataContext = new DataContext(connectionString);
+
+            try
+            {
+                Table<User> users = dataContext.GetTable<User>();
+                var query = from u in users where u.Age > 20 orderby u.Name select u;
+                GetAllData(users);
+                Console.WriteLine(new string('*',20));
+                foreach(var u in query)
+                {
+                    Console.WriteLine($"{u.Id} {u.Name} {u.Age}");
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Error");
+            }
+
+            
+        }
+
+        static void GetAllData(Table<User> users)
+        {
+            foreach(var u in users)
+            {
+                Console.WriteLine($"{u.Id} {u.Name} {u.Age}");
+            }
+        }
+    }
+}
---
+++ Task2Nix/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2Nix
+{
+    [Table(Name = "Users")]
+    class User
+    {
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)]
+        public int Id { get; set; }
+        [Column]
+        public string Name { get; set; }
+        [Column]
+        public int Age { get; set; }
+    }
+}
